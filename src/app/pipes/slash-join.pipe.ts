61b19fa0a3b144eb2088b4d48ffe57ba23f5c010
agentboo <zuzi.han@gmail.com>
2019-04-17 23:11:05 -0400

    added a pipe to join array items with a slash

---
+++ src/app/pipes/slash-join.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'slashJoin'
+})
+export class SlashJoinPipe implements PipeTransform {
+
+  transform(values: string[], args?: any): string {
+    return values.join('/');
+  }
+
+}
