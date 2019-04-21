0a4ca89e43884e09b134105427899fba6a78c81c
agentboo <zuzi.han@gmail.com>
2019-04-21 10:05:29 -0400

    refactored location service

--- src/app/services/location-api.service.ts
+++ src/app/services/location-api.service.ts
@@ -1,27 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Subject }    from 'rxjs';
 import { environment } from '../../environments/environment';
 import { appID } from '../../environments/environment.local';
+
+/**
+ * Location API Service 
+ * Uses OpenWeatherMap to retrieve a list of cities by city name 
+ * OpenWeatherMap recommends '{city},{2-letter country code}' location query format 
+ * However, it is acceptable to use both 'london,uk' and 'london,united+kingdom' formats  
+ * More information at https://openweathermap.org/current
+ */
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationApiService {
-	isFetching = new Subject<string>()
 
   constructor(private http: HttpClient) { }
 
-  private buildURL(location){
+  private buildURL(location: string): string{
   	return `${environment.apiURL}?q=${location}&type=like&sort=population&${appID}`
   }
 
-  retrieveLocation(location){
+  retrieveLocation(location: string){
   	const url = this.buildURL(location)
   	return this.http.get(url)
   }
 
-  doneFetching(status){
-  	this.isFetching.next(status)
-  }
 }
